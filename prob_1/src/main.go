e6c930fce8603e1e249a162719a158943aaebb58
Tom Charlton <tcharlton90@googlemail.com>
2015-08-10 21:57:23 +0100

    just need to smush lists together and remove duplicates

--- prob_1/src/main.go
+++ prob_1/src/main.go
@@ -2,6 +2,45 @@ package main
 
 import "fmt"
 
+func sum(x, y []int) int {
+  // Sums two slices
+  var ans int
+  for _, n := range x {
+    ans += n
+    fmt.Println("%s ", ans)
+  }
+  fmt.Println("---------")
+  for _, n := range y {
+    ans += n
+    fmt.Println("%s ", ans)
+  }
+  return ans
+}
+
+func getMultiples(multiplesOf, limit int) (ans []int){
+  // Gets multiples of a number upto a lmit
+  s := []int{}
+  empty := []int{}
+  for i := 0; i < limit; i++ {
+    if i % multiplesOf == 0 {
+      s = append(s, i)
+    }
+  }
+  if len(s) > 0 {
+    return s
+  } else {
+    return empty
+  }
+}
+
+func runCalc(limit int) (ans int){
+  // Calculates the sum of all multiples of 3 & 5 under limit
+  threes := getMultiples(3, limit)
+  fives  := getMultiples(5, limit)
+  return sum(threes, fives)
+}
+
 func main(){
-  fmt.Printf("Hello world\n")
+  ans := runCalc(1000)
+  fmt.Println("Answer is %d\n", ans)
 }
